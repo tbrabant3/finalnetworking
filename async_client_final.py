fd38d9554610eb7674dabc56c961cc9d0a1173d6
tbrabant3 <tyler.brabant@mymail.champlain.edu>
2018-04-27 20:50:51 -0400

    More final additions

--- async_client_final.py
+++ async_client_final.py
@@ -6,7 +6,25 @@ Prof. Josh Auerbach
 Bare bones example of asynchronously receiving 
 data from server and user input from stdin
 """
+"""async_client_final.py
 
+Author:              Tyler Brabant
+Class:               CSI-235
+Assignment:          Final Project
+Date Assigned:       two weeks prior to end date
+Due Date:            4/27/2018 11:59 PM SKIP DAY ADDED
+
+Description:
+Chat client that sends messages and other information through a protcol defined already.
+
+This code has been adapted from that provided by Prof. Joshua Auerbach: see above
+
+Champlain College CSI-235, Spring 2018
+The following code was written by Tyler Brabant
+
+Special thank you to TJ Leach who helped with the server side and the connection status section
+I did not directly copy his code but he verbally walked me through the concepts of it.
+"""
 import asyncio
 import argparse
 import json
@@ -14,8 +32,13 @@ import struct
 import time
 import ssl
 
+
 class ChatClient(asyncio.Protocol):
     def __init__(self):
+        """
+            Creates member variables
+            returns default member variable values
+        """
         self.buffer = b''
         self.data = b''
         self.length = 0
@@ -23,26 +46,38 @@ class ChatClient(asyncio.Protocol):
         self.status = 2
         # not connected == 0
         # awaiting == 2
-        #  Connected == 1
+        # Connected == 1
         # user provided == 3
         #Source TJ
 
     def data_received(self, data):
-        # print(data)
+        """
+        Receives data from the server and parses it displaying the correct information
+
+        returns the correct information on the screen and shows messages in the order they come
+        all in json format**
+        """
+        # contiuily grabs data
         self.data += data
         if self.length == 0:
             if self.data[:4]:
                 self.length = (struct.unpack('!L', self.data[:4]))[0]
 
-        # print(self.length, len(self.data[4:].decode('UTF-8')))
-
+        # checks the length to make sure it is all there
         if len(self.data[4:].decode('UTF-8')) >= self.length:
             incoming_data = json.loads(self.data[4:self.length + 4].decode('UTF-8'))
-            print(self.data)
             self.buffer = self.data[self.length + 4:]
-            # print("Buffer: ", self.buffer)
         else:
             return
+
+        # Section that checks the json and responds accordingly to the information
+
+        if incoming_data.get("MESSAGES"):
+            messages = incoming_data.get("MESSAGES")
+            for message in messages:
+                print(message[2],
+                      message[0], ": ",
+                      message[3])
 
         if incoming_data.get("USERNAME_ACCEPTED") == True:
             print("Username Accepted.")
@@ -52,6 +87,7 @@ class ChatClient(asyncio.Protocol):
 
         elif incoming_data.get("USERNAME_ACCEPTED") == False:
             print("Username Error.")
+            print(incoming_data)
             if incoming_data.get("INFO"):
                 print(incoming_data.get("INFO"))
             self.status = 2
@@ -60,21 +96,16 @@ class ChatClient(asyncio.Protocol):
             user_list = incoming_data.get("USER_LIST")
             print("Users on server: ", [user for user in user_list])
 
-        if incoming_data.get("MESSAGES"):
-            messages = incoming_data.get("MESSAGES")
-            for message in messages:
-                print(message[2],
-                      message[0], ": ",
-                      message[3])
         if incoming_data.get("USERS_JOINED"):
             print("User ", incoming_data.get("USERS_JOINED")[0], " has joined.")
 
         if incoming_data.get("USERS_LEFT"):
             print("User ", incoming_data.get("USERS_LEFT")[0], " has left.")
 
-        #print(incoming_data)
-        # print("received: ", incoming_data)
+        if incoming_data.get("ERROR"):
+            print("ERROR: ", incoming_data.get("ERROR"))
 
+        # if anything in the buffer keep going
         if self.buffer:
             self.data_received(self.buffer)
         self.buffer = b''
@@ -82,36 +113,41 @@ class ChatClient(asyncio.Protocol):
         self.length = 0
 
     def connection_made(self, transport):
+        """
+        Sets up the connection
+
+        returns the connection transport for the client to use
+        """
         self.transport = transport
         self.address = transport.get_extra_info('peername')
         self.data = b''
         print('Accepted connection from {}'.format(self.address))
 
     def write_out(self, data):
+        """
+        sends out the data with the correct length
+
+        returns the sent data to the server
+        """
         length = struct.pack('!L', len(data))
         data = length + data
         self.transport.write(data)
-        # print(data)
-"""
-    def send_username(self, uname_good):
-        if not uname_good:
-            self.username = input("Please enter your username: ")
-            to_send = {"USERNAME": self.username}
-            client.write_out(json.dumps(to_send).encode('ascii'))
-            print("To send a private message, write the '@' sign first followed by the username"
-                  "and a space to start the message.")
-"""
+
+
 @asyncio.coroutine
 def handle_user_input(loop, client):
     """reads from stdin in separate thread
     
     if user inputs 'quit' stops the event loop
-    otherwise just echos user input
+    otherwise sends the username and then the messages loop and quits when user wants
     """
     while client.status != 1:
         if client.status == 2:
-            print("Please enter your username: ")
+            print("Please enter your username or quit to quit: ")
             client.username = yield from loop.run_in_executor(None, input, "> ")
+            if client.username == "quit":
+                loop.stop()
+                return
             client.status = 3
             to_send = {"USERNAME": client.username}
             client.write_out(json.dumps(to_send).encode('ascii'))
@@ -121,14 +157,16 @@ def handle_user_input(loop, client):
             yield
 
     while True:
+        # sets up the 4 needed aspects of the message
         dest = 'ALL'
         src = client.username
-
         message = yield from loop.run_in_executor(None, input, "> ")
         timestamp = int(time.time())
-        if message == "quit":
+
+        if message == "quit":  # stops the loop
             loop.stop()
             return
+
         if message != "" and message != "@":
             if message[0] == '@':
                 user_to_send = message.split(" ", 1)
@@ -146,8 +184,9 @@ if __name__ == '__main__':
     parser.add_argument('host', help='IP or hostname')
     parser.add_argument('-p', metavar='port', type=int, default=7000,
                         help='TCP port (default 7000)')
-    parser.add_argument('-ca', help='CA file only', nargs='?', default=None)
-
+    parser.add_argument('-ca', help='CA file only', nargs='?', default=None)  # grabs the ca file if not specified
+                                                                              # then it is blank and can connect to
+                                                                              # class server
     args = parser.parse_args()
 
     loop = asyncio.get_event_loop()
@@ -157,6 +196,7 @@ if __name__ == '__main__':
     
     # the lambda client serves as a factory that just returns
     # the client instance we just created
+    # ssl aspects are also here for the security
     purpose = ssl.Purpose.SERVER_AUTH
     context = ssl.create_default_context(purpose, cafile=args.ca)
     coro = loop.create_connection(lambda: client, args.host, args.p, ssl=context)
--- async_server_final.py
+++ async_server_final.py
@@ -1,3 +1,23 @@
+"""async_server_final.py
+
+Author:              Tyler Brabant
+Class:               CSI-235
+Assignment:          Final Project
+Date Assigned:       two weeks prior to end date
+Due Date:            4/27/2018 11:59 PM SKIP DAY ADDED
+
+Description:
+Chat server that receives messages and other information through a protocol defined already.
+it bounces messages back to users who send the information
+
+This code has been adapted from that provided by Prof. Joshua Auerbach: see above
+
+Champlain College CSI-235, Spring 2018
+The following code was written by Tyler Brabant
+
+Special thank you to TJ Leach who helped with the server side and the connection status section
+I did not directly copy his code but he verbally walked me through the concepts of it.
+"""
 import asyncio
 import argparse
 import json
@@ -5,18 +25,17 @@ import struct
 import ssl
 
 
-USER_LIST = []
-USER_LIST_OBJ = []
-"""
-Create dictionary username : ip get the ip address
-
-"""
-MESSAGES = []
+USER_LIST = []  # Usernames
+USER_LIST_OBJ = []  # User objs
+MESSAGES = []  # message touples in storage
 
 
 class ChatServer(asyncio.Protocol):
-
     def __init__(self):
+        """
+           Creates member variables
+           returns default member variable values
+        """
         self.buffer = b''
         self.data = b''
         self.length = 0
@@ -24,9 +43,9 @@ class ChatServer(asyncio.Protocol):
 
     def connection_made(self, transport):
         """
-        Creates the connection
-        :param transport:
-        :return: Returns the accepted connection name and info
+        Sets up the connection
+
+        returns the connection transport for the server object to use
         """
         self.transport = transport
         self.address = transport.get_extra_info('peername')
@@ -35,9 +54,10 @@ class ChatServer(asyncio.Protocol):
 
     def data_received(self, data):
         """
-        Recieves the sent data which are in question delimiters
-        :param data: string
-        :return: writes back the answer
+        Recieves the sent from the client prefaced with the length to check out
+
+         returns to the client the right messages/ information according to what is sent
+         all in json format**
         """
         self.data += data
         if self.length == 0:
@@ -49,7 +69,7 @@ class ChatServer(asyncio.Protocol):
             self.buffer = self.data[self.length + 4:]
         else:
             return
-        print(self.data)
+
         if 'USERNAME' in json_message and json_message.get('USERNAME') not in USER_LIST:
             USER_LIST.append(json_message.get("USERNAME"))
             self.username = json_message.get("USERNAME")
@@ -64,6 +84,13 @@ class ChatServer(asyncio.Protocol):
             user_accept = length + user_accept
             self.transport.write(user_accept)
 
+            # message all the users
+            for user in USER_LIST_OBJ:
+                user_join = json.dumps({"USERS_JOINED": [self.username]}).encode("UTF-8")
+                length = struct.pack('!L', len(user_join))
+                user_join = length + user_join
+                user.transport.write(user_join)
+            print(self.username, " has just joined.")  #lets the server know
         elif 'USERNAME' in json_message and json_message.get('USERNAME') in USER_LIST:
             user_no = json.dumps({"USERNAME_ACCEPTED": False,
                        "Info": "Sorry, Username exists or there is an error."}).encode('UTF-8')
@@ -78,12 +105,18 @@ class ChatServer(asyncio.Protocol):
             user_no = length + user_no
             self.transport.write(user_no)
 
-
         if json_message.get("MESSAGES"):
             messages = json_message.get("MESSAGES")
             for message in messages:
-                MESSAGES.append(message)
-                self.send_to_people(message)
+                # If the user is in the src
+                if self.username == message[0] and message[1] == 'ALL' or message[1] in USER_LIST:
+                    MESSAGES.append(message)
+                    self.send_to_people(message)
+                else:
+                    error_code = json.dumps({"ERROR": "error in message format"}).encode("UTF-8")
+                    length = struct.pack('!L', len(error_code))
+                    error_code = length + error_code
+                    self.transport.write(error_code)
 
         if self.buffer:
             self.data_received(self.buffer)
@@ -94,8 +127,7 @@ class ChatServer(asyncio.Protocol):
     def connection_lost(self, exc):
         """
         Connection lost handling
-        :param exc:
-        :return: Returns the closed socket
+        Returns the connection lost and also sends to the users that the user left
         """
         if exc:
             print('Client {} error: {}'.format(self.address, exc))
@@ -105,20 +137,33 @@ class ChatServer(asyncio.Protocol):
         else:
             print('Client {} closed socket'.format(self.address))
 
+        # removes the user from the list and its obj self from the other list.
         USER_LIST_OBJ.remove(self)
         if self.username in USER_LIST:
             USER_LIST.remove(self.username)
-        print(self.username, " has just left.")
-
+        print(self.username, " has just left.")  # Server message just so it will know
+        for user in USER_LIST_OBJ:
+            user_left = json.dumps({"USERS_LEFT": [self.username]}).encode("UTF-8")
+            length = struct.pack('!L', len(user_left))
+            user_left = length + user_left
+            user.transport.write(user_left)
 
     def send_to_people(self, message):
+        """
+        sends the message to all the users.
+        message is a message in the correct format that is distributed to all users
+
+        returns the correct message to all the right users.
+        """
         if message[1] == "ALL":
+            # sends to all
             for user in USER_LIST_OBJ:
                 to_send = json.dumps({"MESSAGES": [message]}).encode('UTF-8')
                 length = struct.pack('!L', len(to_send))
                 to_send = length + to_send
                 user.transport.write(to_send)
         else:
+            # Sends to individuals
             for user in USER_LIST_OBJ:
                 if message[1] == user.username:
                     to_send = json.dumps({"MESSAGES": [message]}).encode('UTF-8')
@@ -136,6 +181,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     address = (args.host, args.p)
 
+    # adds security to the server and the file must be added to opperate
     purpose = ssl.Purpose.CLIENT_AUTH
     context = ssl.create_default_context(purpose)
     context.load_cert_chain(args.pem)
